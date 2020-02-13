72072157cbfd35afee34cd8bcfebf706d9806f3c
qinglingzhiyu <1012173543@qq.com>
2020-02-13 22:06:14 +0800

    Updates

---
+++ precache-manifest.6d53deb6e3926b7a67e67d627081fdbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a07046880dd5181e50ec8893b0a1e4f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1299b8ff7424457fa73b",
+    "url": "./static/css/main.7feb39b5.chunk.css"
+  },
+  {
+    "revision": "757d6e7cd4b28aa06628",
+    "url": "./static/js/2.c1b3cf1d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.c1b3cf1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1299b8ff7424457fa73b",
+    "url": "./static/js/main.d1ed43d4.chunk.js"
+  },
+  {
+    "revision": "d8a1d70336cf5f9e7910",
+    "url": "./static/js/runtime-main.8afa82e3.js"
+  }
+]);
