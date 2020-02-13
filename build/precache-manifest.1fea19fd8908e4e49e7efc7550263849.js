3856ae5cb08061559c0a4318c0bf6c38ff13998f
qinglingzhiyu <1012173543@qq.com>
2020-02-13 22:57:57 +0800

    update

--- build/precache-manifest.1fea19fd8908e4e49e7efc7550263849.js
+++ build/precache-manifest.1fea19fd8908e4e49e7efc7550263849.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2a07046880dd5181e50ec8893b0a1e4f",
+    "revision": "1edbc94ed5ef32d4fe0ccbb32f4c5ff1",
     "url": "./index.html"
   },
   {
-    "revision": "1299b8ff7424457fa73b",
-    "url": "./static/css/main.7feb39b5.chunk.css"
+    "revision": "5e24172e353736f42566",
+    "url": "./static/css/main.b34d981b.chunk.css"
   },
   {
     "revision": "757d6e7cd4b28aa06628",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.c1b3cf1d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1299b8ff7424457fa73b",
-    "url": "./static/js/main.d1ed43d4.chunk.js"
+    "revision": "5e24172e353736f42566",
+    "url": "./static/js/main.a480f326.chunk.js"
   },
   {
     "revision": "d8a1d70336cf5f9e7910",
